84586e441047d186459d6520ea95a725d7ff6da1
Swapnil Sahu <swapnilsahu4@gmail.com>
2022-07-22 03:25:02 +0530

    1.Made some style changes to card 2.added loading spinner for images

--- src/component/Photo.js
+++ src/component/Photo.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+
+import "./Photo.css";
+
 export default function Photo ({item}) {
-    return <div>
+    const [isLoading, setIsLoading] = useState(true);
+
+    return <div className="card">
         <p>{item.title}</p>
-        <img src={item.src} alt={item.title} style={{margin: '4px'}} />
+        <img src={item.src} alt={item.title} style={{margin: '4px'}} onLoad={()=>{setIsLoading(false)}}/>
+        {isLoading && <span className="loader"></span>}
     </div>
 }
