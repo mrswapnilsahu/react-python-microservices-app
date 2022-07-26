a8cc0a289ee1701b8a7f98cb26dfc73ce371aaf4
Swapnil Sahu <swapnilsahu4@gmail.com>
2022-07-26 19:25:34 +0530

    test msw config

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// if (process.env.NODE_ENV === 'development') {
-    const { worker } = require('./mocks/browser')
-    worker.start()
-// }
+    if (
+        process.env.REACT_APP_BUILD_MODE === "serve" ||
+        process.env.REACT_APP_BUILD_MODE === "gh-pages" ||
+        process.env.NODE_ENV === "development"
+      ) {
+
+        const { worker } = require('./mocks/browser')
+        worker.start();
+      }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
